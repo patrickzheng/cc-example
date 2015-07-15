b6de144765598c6aeb04e975083cfd5b509429b3
Patrick Zheng <PatrickZheng@Patricks-MBP.attlocal.net>
2015-07-14 22:43:21 -0700

    median_unique and words_tweeted are functioning now

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,1 +1,29 @@
 # example of program that calculates the median number of unique words per tweet.
+import sys
+def median_unique(inputFilename, outputFilename):
+    countDict = {}
+    outputFile = open(outputFilename,'w')
+    with open(inputFilename) as inputFile:
+        for line in inputFile:
+            tweetWordCount = len(line.split( ))
+            if tweetWordCount not in countDict.keys():
+                countDict[tweetWordCount] = 1
+            else:
+                countDict[tweetWordCount] = countDict[tweetWordCount] + 1  
+            medianPosition = float(sum(countDict.values()))/2
+            medianValue1 = 0
+            medianValue2 = 0
+            for key in sorted(countDict.keys()):
+                medianPosition = medianPosition - countDict[key];
+                if medianPosition == 0: 
+                    medianValue1 = key
+                elif medianPosition < 0:
+                    medianValue2 = key
+                    if medianValue1 == 0:
+                        medianValue1 = key
+                    break
+            outputFile.write(str(float(medianValue1 + medianValue2) / 2) + '\n')
+    outputFile.close()
+inputFilename = sys.argv[1]
+outputFilename = sys.argv[2]
+median_unique(inputFilename, outputFilename)
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,1 +1,19 @@
 # example of program that calculates the total number of times each word has been tweeted.
+import sys
+def words_tweeted(inputFilename, outputFilename):
+    wordDict = {}
+    with open(inputFilename) as f:
+        for line in f:
+            for word in line.split( ):
+                if word not in wordDict.keys():
+                    wordDict[word] = 1
+                else:
+                    wordDict[word] = wordDict[word] + 1
+    f = open(outputFilename,'w')
+    for key in sorted(wordDict.keys()):
+        #print key + ' ' + str(wordDict[key])
+        f.write(key + ' ' + str(wordDict[key]) + '\n')
+    f.close()
+inputFilename = sys.argv[1]
+outputFilename = sys.argv[2]
+words_tweeted(inputFilename, outputFilename)
